b52cd7e277012f55c3fca139f98f92693bb6194c
V. Ramesh-Lalchand <vr308@aura.lsc.phy.private.cam.ac.uk>
2017-11-14 23:13:20 +0000

    edits

--- bayesian_regression.py
+++ bayesian_regression.py
@@ -14,7 +14,18 @@ import scipy.stats as st
 import scipy.optimize as so
 import statsmodels.api as smt
 import pandas as pd
+from scipy.stats import gaussian_kde
 
+def distSquared(X,Y):
+    nx = np.size(X, 0)
+    ny = np.size(Y, 0)
+    D2 =  (np.multiply(X, X).sum(1)  * np.ones((1, ny)) ) + ( np.ones((nx, 1)) * np.multiply(Y, Y).sum(1).T  ) - 2*X*Y.T
+    return D2
+
+
+def determine_gaussian_basis():
+    
+    
 
 def predictive_distribution():
     
@@ -26,13 +37,42 @@ def posterior_update():
 
 if __name__ == "__main__":
 
-    x = np.linspace(1,20,100)
-    y_true = 0.5*np.sin(x) + 0.5*x + -0.02*(x-5)**2
-    y_noise = y_true + 0.2*np.std(y_true)*np.random.normal(0,1,100)
+    N = 100
+    u = np.sort(np.random.uniform(-10,10,N))
+    x = np.matrix(u).T
+    y_true = np.matrix(5*np.sin(u) + 0.5*u + -0.2*np.square((u-5))).T
     
+    noiseToSignal = 0.2
+    noise = np.std(y_true, ddof=1) * noiseToSignal
+    y_noise = y_true + noise*np.random.randn(N,1)
+
     plt.figure()
     plt.plot(x,y_true, 'b')
-    plt.scatter(x,y_noise, s=4)
+    plt.plot(x,y_noise, 'ko',markersize=2)
     
+    #Define Basis Matrix
+    
+    bw = 0.2
+    BASIS = np.matrix(np.exp(-distSquared(x,x)/(bw**2)))
+    plt.plot(x,BASIS)
+    
+    # Define the parameters of the prior
+    
+    # Normal Prior 
+    
+    mu = 
+    Sigma =
+    
+   # Sparsity Prior 
+    
+   from sklearn.neighbors import NearestNeighbors
+   X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+   nbrs = NearestNeighbors(n_neighbors=3, algorithm='ball_tree').fit(X)
+   distances, indices = nbrs.kneighbors(X)
+   
+   kde = gaussian_kde(u, bw_method=0.2)
+   density = kde.evaluate(u)
+   plt.plot(u,density)
+   plt.hist(u, normed=True)
     
     
