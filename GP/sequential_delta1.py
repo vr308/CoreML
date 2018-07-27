9af6c33773aab57adabe2f3252ded8dfd3b651db
V. Ramesh-Lalchand <vr308@aura.lsc.phy.private.cam.ac.uk>
2018-07-27 21:39:49 +0100

    edits

--- GP/sequential_delta1.py
+++ GP/sequential_delta1.py
@@ -29,69 +29,15 @@ import numpy as np
 
 def f(x):
     """The function to predict."""
-    return np.square(x)*np.sin(x)
+    #return np.square(x)*np.sin(x)
     #return x*np.sin(x)
-    #return 0.5*np.sin(x) + 0.5*x -0.02(x-5)^2
+    return 0.5*np.sin(x) + 0.5*x -0.02*np.square(x-5)
     #return -np.cos(np.pi*x) + np.sin(4*np.pi*x)
 
 def f2(x,y):
     """ The 2 d function to predict """
     return x*np.exp(-np.square(x)-np.square(y))
 
-
-#def plot_gp_fit_predict(X_train, x, y_train, y_pred, sigma, knots, y_pred_knots, sigma_knots, title, gpr, entropy):
-#    
-#    #y_upper = y_pred.reshape(len(x),) + 2*sigma
-#    #y_lower = y_pred.reshape(len(x),) - 2*sigma
-#    
-#    #upper_coords = np.column_stack((x, y_upper))
-#    #lower_coords = np.column_stack((x, y_lower))
-#
-#    #dist_matrix = distance.cdist(upper_coords, lower_coords, metric='euclidean')
-#
-#    #z=[np.min(dist_matrix[i]) for i in np.arange(len(upper_coords))]
-#    
-#    #cmap = cm.get_cmap('viridis')
-#    #x_array = x.reshape(len(y_pred),)
-#    
-#    plt.figure()
-#    plt.plot(x, f(x), 'r:', label=u'$f(x) = x^2\sin(x)$')
-#    plt.plot(X_train, y_train, 'r.', markersize=10, label=u'Training Observations')
-#    plt.plot(x, y_pred, 'b-', label=u'Prediction')
-#    plt.fill_between(x.reshape(len(x),), y_pred.reshape(len(x),) - 1.96*sigma, y_pred.reshape(len(x),) + 1.96*sigma, alpha=0.5, color='y')
-#    
-#    #normalize = mpl.colors.Normalize(min(sigma), max(sigma))
-#    #for i in range(999):
-#    #    plt.fill_between([x_array[i],x_array[i+1]], y_lower[i], y_upper[i], color = cmap(normalize(sigma[i])), alpha=0.7)
-#    #for i in range(10):
-#    #    plt.plot(x, gpr.sample_y(x, 10)[i][0], alpha=0.5, linewidth=0.5)
-#    #if entropy:
-#    #    plt.errorbar(knots, y_pred_knots, yerr=sigma_knots*2, barsabove=True, ls='None', marker='s', capsize=10, color='orange')
-#    plt.xlabel('$x$')
-#    plt.ylabel('$f(x)$')
-#    plt.ylim(-300, +300)
-#    #plt.legend(loc='upper left')
-#    plt.title(title, fontsize='small')
-#    #plt.annotate('Knots at ' + str(knots), xy=[5,8])
-        
-def get_max_entropy(sigma_, X):
-    
-    pos_max_sigma = np.where(np.round(sigma_,4) == np.round(np.max(sigma_),4))[0]
-    if len(pos_max_sigma) > 1:
-        x_ = X[pos_max_sigma].reshape(len(pos_max_sigma),)
-        if np.where(np.diff(pos_max_sigma) > 1)[0].size > 0:
-            print('There are more than 1 regions of max entropy, identifying the centers of each region')
-            break_points = np.where(np.diff(pos_max_sigma) > 1)[0]
-            regions = np.split(x_,break_points+1)
-            return [np.round(np.median(k),2) for k in regions]
-        else:
-            print('There is 1 region of max entropy, returning its center')
-            return [np.round(np.median(x[pos_max_sigma]),2)]
-    else:
-        print('There is one distinct point of highest variance')
-        return [np.round(np.median(x[pos_max_sigma]),2)]
-    
-    
 def get_data_max_var(sigma_, X):
       
     sigma_ = np.round(sigma_,4)
@@ -120,16 +66,15 @@ def plot_mle_trace(mle_trace, mean_mle_trace):
     plt.plot(mean_mle_trace, label = 'Uniform Centers')
     plt.legend(fontsize='small')
     
-    
-def plot_seq_gp():
-    return
 
 if __name__ == "__main__":
     
     X = np.atleast_2d(np.sort(np.random.uniform(0,+20,40))).T
-    x = np.atleast_2d(np.linspace(0, 20, 1000)).T
+    #x = np.atleast_2d(np.linspace(0, 2, 1000)).T
+    x = np.atleast_2d(np.linspace(0,20, 1000)).T
+    y_test = f(x) + np.random.normal(0, 1, len(x)).reshape(len(x),1)
 
-    noise = np.random.normal(0, 20, len(X)).reshape(len(X),1)
+    noise = np.random.normal(0, 1, len(X)).reshape(len(X),1)
     y = f(X) + noise
         
     i = 1
@@ -151,7 +96,8 @@ if __name__ == "__main__":
         y_remainder = y[~bool_array]
         
         # Instansiate a Gaussian Process model
-        kernel = Ck(1000.0, (1e-3, 1e4)) * RBF(1.0, (1, 1e2)) + WhiteKernel(noise_level=10, noise_level_bounds="fixed")
+        #kernel = Ck(1000, (1e-3, 1e4)) * RBF(1.0, (1, 1e2)) + WhiteKernel(noise_level=10, noise_level_bounds="fixed")
+        kernel = Ck(10, (1e-3, 1e4)) * RBF(2.0, (1, 1e2)) + WhiteKernel(noise_level=1, noise_level_bounds="fixed")
         gpr = GaussianProcessRegressor(kernel=kernel,optimizer=None)
         
         # Fit to data using Maximum Likelihood Estimation of the parameters
@@ -226,7 +172,7 @@ if __name__ == "__main__":
         if delta == 2:
             plt.vlines(np.ravel(X), ymin=y_min_array, ymax=y_max_array,alpha=0.7, color='y', label='$|\mu_{t} - y(R_{t})|$')
         elif delta == 3:
-            plt.errorbar(np.ravel(X_remainder), y_remainder, yerr=factor[~bool_array] , fmt='none', ecolor='orange')
+            plt.errorbar(np.ravel(X_remainder), y_remainder, yerr=factor[~bool_array] , fmt='none', ecolor='orange', alpha=0.5)
             #plt.vlines(np.ravel(X), ymin=y_min_factor_array, ymax=y_max_factor_array, alpha=0.7, color='y', label=r'$\sqrt{diag(\Sigma_{t})} + |\mu_{t} - y(R_{t})|$')
         plt.title('GPR with progressive training ' + '[Stage t = ' + str(len(X_active)) + ']' + '\n' + r'$RMSE (X(R_{t})): $' +  str(rmse_remainder), fontsize='x-small')
         #plt.legend(fontsize='x-small')
@@ -235,13 +181,16 @@ if __name__ == "__main__":
             
         if i > 1:
             delta_error = remainder_err[-2] - remainder_err[-1]
-            if (delta_error < 0.05*(np.max(X) - np.min(X)) and delta_error > 0):
+            print(delta_error)
+            #if (delta_error < 0.05*(np.max(X) - np.min(X)) and delta_error > 0):
+            if (delta_error < 0.05 and delta_error > 0):
+            #if (i == 7):
                 
                 print('Training has converged')
                 print('Conducting Hyper-parameter optimization')
                 print ('Initial Kernel: ' + str(gpr.kernel_))
                 
-                rmse_test = np.round(np.sqrt(np.mean(np.square(y_pred - f(x)))),2)
+                rmse_test = np.round(np.sqrt(np.mean(np.square(y_pred - y_test))),2)
                 print ('Test error on hold out set: ' + str(rmse_test))
                 
                 #gpr = GaussianProcessRegressor(kernel=kernel)
@@ -252,18 +201,18 @@ if __name__ == "__main__":
                 #y_pred_opt, sigma = gpr.predict(x, return_std=True)
                    
                 # Plot the evolution of test error
-#                plot_factor = np.mod(i,8)
-#                if(plot_factor == 0):
-#                    plt.figure(figsize=(15,8))
-#                    plt.subplot(2, 4, np.mod(i+1,8))  
-#                else:
-#                    plt.subplot(2, 4, np.mod(i+1,8))
-#                    
-#                #plt.subplot(2, 4, np.mod(i+1,8))  
-#                #plt.figure()
-#                plt.plot(remainder_err, label='Test error')
-#                plt.legend(fontsize = 'x-small')
-#                plt.title('Evolution of Remainder Error ' + str(len(X_active)) + ' active points ', fontsize='small')
+                plot_factor = np.mod(i,8)
+                if(plot_factor == 0):
+                    plt.figure(figsize=(15,8))
+                    plt.subplot(2, 4, np.mod(i+1,8))  
+                else:
+                    plt.subplot(2, 4, np.mod(i+1,8))
+                    
+                plt.subplot(2, 4, np.mod(i+1,8))  
+                #plt.figure()
+                plt.plot(remainder_err, label='Remainder error')
+                plt.legend(fontsize = 'x-small')
+                plt.title('Evolution of Remainder Error ' + str(len(X_active)) + ' active points ' + '\n' + 'Test error: ' + str(rmse_test), fontsize='x-small')
                 
                 #if (np.mod(i+2,8) == 0):
                 #     last_plot = 8
@@ -285,26 +234,35 @@ if __name__ == "__main__":
         i = i + 1
             
 # Testing out on hold out set
-
-gpr = GaussianProcessRegressor(kernel=kernel)
+        
+kernel = Ck(10, (1e-3, 1e4)) * RBF(2.0, (1, 1e2)) + WhiteKernel(noise_level=1, noise_level_bounds="fixed")
+gpr = GaussianProcessRegressor(kernel=kernel,optimizer=None)
 
 #Full GP
 
 gpr_full = gpr.fit(X,y) 
 y_pred_full, sigma_full = gpr_full.predict(x, return_std = True)
+rmse_test = np.round(np.sqrt(np.mean(np.square(y_pred_full - y_test))),4)
+print(rmse_test)
 
-# Random subset GP 
+plt.figure()
+plt.plot(X, y, 'ro', markersize=1, label='Noisy data')
+plt.plot(x, f(x), 'r', alpha=0.3, label='True function')
+plt.plot(x, y_pred_full, label='Mean ' + r'$\mu_{t}$')
+plt.fill_between(np.ravel(x), np.ravel(y_pred_full) - 2*sigma_full, np.ravel(y_pred_full) + 2*sigma_full, alpha=0.2, color='c', label=r'$2 \sqrt{diag(\Sigma_{t})}$')
 
-gpr_subset = gpr.fit()
+# Random subset GP
+
+idx = [0,3,7,11,14,15,17,21,29,31,32,36,39]
  
+X_sub = X[idx]
+y_sub = y[idx]
 
-y_pred_pgp, sigma = gpr.predict(x, return_std=True)
-test_err_pgp = np.sqrt(np.square(f(x) - y_pred_pgp))
-
-
-
-
-        
+gpr_subset = gpr.fit(X_sub, y_sub)
+y_pred_sub, sigma_sub = gpr_subset.predict(x, return_std = True)
+rmse_test = np.round(np.sqrt(np.mean(np.square(y_pred_sub - y_test))),4)
+print(rmse_test)
+ 
 
 #pdf = matplotlib.backends.backend_pdf.PdfPages("/home/raid/vr308/PHD/Code/Explorations/GP/Exp6.pdf")
 #for fig in xrange(1, plt.get_fignums()[-1] +1): ## will open an empty extra figure :(
