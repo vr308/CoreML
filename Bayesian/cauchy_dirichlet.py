dff979940d3ce02557199e7a85732c126074c516
V. Ramesh-Lalchand <vr308@aura.lsc.phy.private.cam.ac.uk>
2018-08-20 14:28:45 +0100

    edits

--- Bayesian/cauchy_dirichlet.py
+++ Bayesian/cauchy_dirichlet.py
@@ -12,8 +12,8 @@ from scipy.stats import beta
 import matplotlib.pylab as plt
 from statsmodels.nonparametric.kde import KDEUnivariate
 
-c1 = st.cauchy.rvs(loc=-1,scale=2, size=1000)
-c2 = st.cauchy.rvs(loc=4,scale=1.2, size=1000)
+c1 = st.cauchy.rvs(loc=-1,scale=2, size=100)
+c2 = st.cauchy.rvs(loc=4,scale=1.2, size=100)
 
 x_grid = np.linspace(-4,10, 10000)
 pdf1= st.cauchy.pdf(x_grid, -1, 2)
@@ -31,7 +31,7 @@ c = np.ravel(np.vstack((c1,c2)))
 plt.hist(c, bins=100, density=True, alpha=0.4)
 
 kde = KDEUnivariate(c)
-kde.fit(bw=1)
+kde.fit(bw=0.8)
 
 plt.plot(x_grid, kde.evaluate(x_grid))
 
@@ -201,6 +201,48 @@ ax.set_xlabel('Yearly sunspot count');
 ax.set_yticklabels([]);
 ax.legend(loc=1);
 
+##Another example
 
+old_faithful_df = pd.read_csv(pm.get_data('old_faithful.csv'))
+old_faithful_df['std_waiting'] = (old_faithful_df.waiting - old_faithful_df.waiting.mean()) / old_faithful_df.waiting.std()
+
+K = 30
+N = 318
+
+with pm.Model() as model:
+    alpha = pm.Gamma('alpha',1,1)
+    beta = pm.Beta('beta', 1, alpha, shape=K)
+    w = pm.Deterministic('w', stick_breaking(beta))
     
+    tau = pm.Gamma('tau', 1., 1., shape=K)
+    lambda_ = pm.Uniform('lambda', 0, 5, shape=K)
+    mu = pm.Normal('mu', 0, tau=lambda_ * tau, shape=K)
+    obs = pm.NormalMixture('obs', w, mu, tau=lambda_ * tau,
+                           observed=old_faithful_df.std_waiting.values)
 
+with model:
+    trace = pm.sample(500)
+
+post_pdf_contribs = sp.stats.norm.pdf(np.atleast_3d(x_plot),
+                                      trace['mu'][:, np.newaxis, :],
+                                      1. / np.sqrt(trace['lambda'] * trace['tau'])[:, np.newaxis, :])
+post_pdfs = (trace['w'][:, np.newaxis, :] * post_pdf_contribs).sum(axis=-1)
+post_pdf_low, post_pdf_high = np.percentile(post_pdfs, [2.5, 97.5], axis=0)
+
+fig, ax = plt.subplots(figsize=(8, 6))
+x_plot = np.linspace(-3, 3, 200)
+n_bins = 20
+
+ax.hist(old_faithful_df.std_waiting.values, bins=n_bins, normed=True,
+        color='b', lw=0, alpha=0.5);
+ax.fill_between(x_plot, post_pdf_low, post_pdf_high,
+                color='gray', alpha=0.45);
+ax.plot(x_plot, post_pdfs[0],
+        c='gray', label='Posterior sample densities');
+ax.plot(x_plot, post_pdfs[::100].T, c='gray');
+ax.plot(x_plot, post_pdfs.mean(axis=0),
+        c='k', label='Posterior expected density');
+ax.set_xlabel('Standardized waiting time between eruptions');
+ax.set_yticklabels([]);
+ax.set_ylabel('Density');
+ax.legend(loc=2);
