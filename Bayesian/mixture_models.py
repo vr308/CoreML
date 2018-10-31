f20a860a689fda892eccbc2c63b8f9a586d9dea4
V. Ramesh-Lalchand <vr308@aura.lsc.phy.private.cam.ac.uk>
2018-10-31 20:32:32 +0000

    edits

--- Bayesian/mixture_models.py
+++ Bayesian/mixture_models.py
@@ -14,9 +14,9 @@ from sklearn.mixture import GaussianMixture
 
 # Generating some random variates
 
-m1 = st.norm.rvs(-2, 1, 300)
-m2 = st.norm.rvs(7,2,400)
-m3 = st.norm.rvs(2,0.7,200)
+m1 = st.norm.rvs(-2, 1, 50)
+m2 = st.norm.rvs(0.5,1.4,50)
+m3 = st.norm.rvs(4,0.9,50)
 
 data = np.concatenate((m1,m2,m3)).reshape(-1,1)
 
@@ -25,8 +25,8 @@ data = np.concatenate((m1,m2,m3)).reshape(-1,1)
 x_ = np.linspace(-6,12,1000)
 
 d1 = st.norm.pdf(x_,-2,1)
-d2 = st.norm.pdf(x_,7,2)
-d3 = st.norm.pdf(x_, 2, 0.7)
+d2 = st.norm.pdf(x_,0.5,1.4)
+d3 = st.norm.pdf(x_, 4, 0.7)
 
 mixture = 0.4*d1 + 0.2*d2 + 0.4*d3
 
@@ -39,54 +39,52 @@ plt.plot(m1, [0]*len(m1), 'kx', markersize=1)
 plt.plot(m2, [0]*len(m2), 'kx', markersize=1)
 plt.plot(m3, [0]*len(m3), 'kx', markersize=1)
 
-def plot_results(data, x_,true_density, estimator):
+def plot_results(data, x_,true_density, estimator, model):
     
     weights = estimator.weights_
-    #n_comp = len(weights)
     means = estimator.means_
     covariances = estimator.covariances_
-    estimated_density = weights[0]*st.norm.pdf(x_, means[0], np.sqrt(covariances[0])) + weights[1]*st.norm.pdf(x_, means[1], np.sqrt(covariances[1]))
+    components = []
+    for counter, value in enumerate(zip(means, covariances)):
+            components.append(weights[counter]*st.norm.pdf(x_, value[0], np.sqrt(value[1])).reshape(1000,))     
+    estimated_density = np.sum(components, axis=0)
     plt.figure()
-    plt.plot(x_,true_density, 'k', alpha=0.2)
-    plt.plot(x_, 0.4*d1, 'b--', markersize=1)
-    plt.plot(x_, 0.2*d2, 'g--', markersize=1)
-    plt.plot(x_, 0.4*d3, 'r--', markersize=1)
-    plt.plot(x_, estimated_density[0], alpha=0.7)
-    plt.plot(data, [0]*len(data), 'kx', markersize=2)
+    plt.plot(x_,true_density, 'k', alpha=0.2, label='True Density')
+    plt.plot(x_, estimated_density, alpha=0.7, label='Estimated Density')
+    plt.plot(data, [0]*len(data), 'kx', markersize=2, label='Observations')
+    plt.title(model + ' K = ' + str(len(weights)), fontsize='small')
+    plt.legend(fontsize='small')
     
 # Gaussian non-Bayesian mixture
-estimator_gmm = GaussianMixture(n_components=2, 
+estimator_gmm = GaussianMixture(n_components=5, 
                                 covariance_type='full',
                                   verbose=1, n_init=5).fit(data)
                                 
 
-estimator = estimator_gmm
-plot_results(data, x_, mixture, estimator_gmm)
-
-
 # Finite Bayesian mixture
 
-estimator_bgmm = BayesianGaussianMixture(n_components=4, 
+estimator_bgmm = BayesianGaussianMixture(n_components=5, 
                                     covariance_type='full', 
-                                    init_params='random',
                                     weight_concentration_prior_type='dirichlet_distribution',
                                     weight_concentration_prior=None,
                                     verbose=1).fit(data)
-
 
 # Infinite Bayesian mixture 
 
 estimator_dpgmm = BayesianGaussianMixture(n_components=4, 
                                     covariance_type='full', 
-                                    init_params='random',
                                     weight_concentration_prior_type='dirichlet_process',
-                                    weight_concentration_prior=None,
+                                    weight_concentration_prior=1000,
                                     verbose=1).fit(data)
 
+plot_results(data, x_, mixture, estimator_gmm, 'GMM, Inference: EM')
+plot_results(data, x_, mixture, estimator_bgmm, 'Bayesian GMM')
+plot_results(data, x_, mixture, estimator_dpgmm, 'Dirichlet Process prior GMM')
 
 
-
-
+# Sample posterior densities from estimated mixtures - in the sklearn framework 
+# Construct a density from a histogram and sample from it
+# Fit DPGMM and BGM to the cms data.
 
 # 2d example from sklearn website
 
