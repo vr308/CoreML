59c14ed73dfa3d7f836d3ea9ad5f954fccfaf5d7
Vidhi Lalchand <vr308@cam.ac.uk>
2019-01-30 09:29:21 +0000

    edits

--- GP/Hyperparameter Integration/gp_hyp_analysis.py
+++ GP/Hyperparameter Integration/gp_hyp_analysis.py
@@ -46,7 +46,7 @@ mean = pm.gp.mean.Zero()
 
 # The latent function values are one sample from a multivariate normal
 f = np.random.multivariate_normal(mean(X).eval(), cov=K.eval()).flatten()
-y = f +  noise_var*np.random.rand(n)
+y = f + np.random.normal(0, scale=np.sqrt(noise_var), size=n)
 
 # Plot the data and the unobserved latent function
 fig = plt.figure(figsize=(12,5)); 
@@ -65,16 +65,60 @@ v = np.linalg.solve(L, K_s.eval())
 post_mean = np.dot(K_s.T.eval(), alpha)
 
 post_cov = K_ss.eval() - K_s.T.dot(K_inv).dot(K_s)
+post_std = np.sqrt(np.diag(post_cov.eval()))
 #ost_cov = K_ss.eval() - np.dot(v.T, v)
 
 samples = np.random.multivariate_normal(post_mean, post_cov.eval(), 10)
 
 plt.figure()
-plt.plot(X, f, "dodgerblue", lw=3, label="True f",alpha=0.7);
-plt.plot(X, y, 'ox', ms=3, alpha=0.5, label="Data");
-plt.plot(X_star, samples.T, color='red', alpha=0.2)
+plt.plot(X_star, samples.T, color='orange', alpha=0.5)
+plt.plot(X, f, "dodgerblue", lw=1.4, label="True f",alpha=0.7);
+plt.plot(X, y, 'rx', ms=3, alpha=0.8, color='r');
+plt.plot(X_star, post_mean, color='r', lw=2, label='Posterior mean')
+plt.fill_between(np.ravel(X_star), post_mean - 1.96*post_std, 
+                 post_mean + 1.96*post_std, alpha=0.2, color='r',
+                 label='95% CR')
+plt.legend(fontsize='x-small')
+plt.title('GPR')
 
+# Vanilla analytical GP in pymc3
+
+with pm.Model() as latent_gp_model:
+    # Specify the covariance function.
+    cov_func = pm.gp.cov.ExpQuad(1, ls=1)
+
+    # Specify the GP.  The default mean function is `Zero`.
+    gp = pm.gp.Latent(cov_func=cov_func)
+
+    # Place a GP prior over the function f.
+    f = gp.prior("f", X=X)
+    
+    f_star = gp.conditional("f_star", X_star)
+    
+    plt.figure()
+    plt.plot(X, f, "dodgerblue", lw=1.4, label="True f",alpha=0.7);
+    plt.plot(X_star, f_star, color='r', lw=2, label='Posterior mean')
+   
+    
+    
 
 # Type II ML estimation 
+
+
+
+# HMC Sampling -> lengthscale
+
+
+
+
+# HMC Sampling -> noise var
+
+
+
+
+
+# HMC Sampling -> signal var
+
+
 
 
