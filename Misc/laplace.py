da941a8a5d3710ad6c325c72ad322710cf2cad1b
vr308 <vidhilalchand@gmail.com>
2019-03-15 18:27:01 +0000

    edits

--- Misc/laplace.py
+++ Misc/laplace.py
@@ -8,4 +8,13 @@ Created on Thu Mar 14 16:25:32 2019
 Laplace approximation for arbitrary distributions in 1d and 2d
 
 """
+import pymc3 as pm
+import scipy.stats as st
+import tensorflow as tf
+import tensorflow_probability as tfp
 
+with pm.Model() as gm:
+      
+      
+      
+      
