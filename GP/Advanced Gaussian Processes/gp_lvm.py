f1a951f2a18ef75f579d2ab2c37aec24a7414c4f
vr308 <vidhilalchand@gmail.com>
2019-11-23 23:10:32 +0000

    edits

--- GP/Advanced Gaussian Processes/gp_lvm.py
+++ GP/Advanced Gaussian Processes/gp_lvm.py
@@ -6,18 +6,24 @@ Created on Tue Oct 29 15:26:15 2019
 @author: vidhi
 """
 
-import os
-import matplotlib.pyplot as plt
-import pandas as pd
-import torch
-from torch.nn import Parameter
+import numpy as np
+%matplotlib inline
+from matplotlib import pyplot as plt
+import GPy # import GPy package
+np.random.seed(12345)
 
-import pyro
-import pyro.contrib.gp as gp
-import pyro.distributions as dist
-import pyro.ops.stats as stats
+# Define dataset 
+N = 100
+k1 = GPy.kern.RBF(5, variance=1, lengthscale=1./np.random.dirichlet(np.r_[10,10,10,0.1,0.1]), ARD=True)
+k2 = GPy.kern.RBF(5, variance=1, lengthscale=1./np.random.dirichlet(np.r_[10,0.1,10,0.1,10]), ARD=True)
+k3 = GPy.kern.RBF(5, variance=1, lengthscale=1./np.random.dirichlet(np.r_[0.1,0.1,10,10,10]), ARD=True)
+X = np.random.normal(0, 1, (N, 5))
+A = np.random.multivariate_normal(np.zeros(N), k1.K(X), 10).T
+B = np.random.multivariate_normal(np.zeros(N), k2.K(X), 10).T
+C = np.random.multivariate_normal(np.zeros(N), k3.K(X), 10).T
 
-smoke_test = ('CI' in os.environ)  # ignore; used to check code integrity in the Pyro repo
-assert pyro.__version__.startswith('0.4.1')
-pyro.enable_validation(True)       # can help with debugging
-pyro.set_rng_seed(1)
+Y = np.vstack((A,B,C))
+labels = np.hstack((np.zeros(A.shape[0]), np.ones(B.shape[0]), np.ones(C.shape[0])*2))
+
+input_dim = 2 # How many latent dimensions to use
+kernel = GPy.kern.RBF(input_dim, 1, ARD=True) 
