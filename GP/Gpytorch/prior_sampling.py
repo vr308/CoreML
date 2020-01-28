a8f6069b676778db62f327ad938efdc7e5abed75
vr308 <vidhilalchand@gmail.com>
2020-01-28 22:30:00 +0000

    edits

--- GP/Gpytorch/prior_sampling.py
+++ GP/Gpytorch/prior_sampling.py
@@ -6,8 +6,8 @@ Created on Mon Jan 27 14:52:00 2020
 @author: vidhi
 """
 
-import math
 import numpy as np
+import math
 import torch
 import gpytorch
 from matplotlib import pyplot as plt
@@ -17,10 +17,19 @@ from matplotlib import pyplot as plt
 x = torch.linspace(0.01,10,200)
 
 lin_covar = gpytorch.kernels.LinearKernel(num_dimensions=1)
-poly_covar = gpytorch.kernels.PolynomialKernel(power=1)
-se_covar = gpytorch.kernels.RBFKernel()
+poly_covar = gpytorch.kernels.PolynomialKernel(power=4)
+
+se_covar = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel())
+se_covar.lenghtscale = 5
+se_covar.outputscale = 10
+
 per_covar = gpytorch.kernels.PeriodicKernel()
+per_covar.lenghtscale = 3
+per_covar.period_length = 2
+
 rq_covar = gpytorch.kernels.RQKernel()
+rq_covar.lengthscale = 2
+rq_covar.alpha = 10
 
 # Creating the kernel matrix
 
@@ -41,15 +50,46 @@ plt.matshow(K_rq.detach())
 
 # Drawing functions from kernels with fixed hyperparameters (Forward sampling)
 
+def get_samples(covar):
 
+    gp_prior = gpytorch.distributions.MultivariateNormal(mean=torch.tensor([0]*200), covariance_matrix=covar, validate_args=True)
+    samples = gp_prior.sample(sample_shape=torch.Size([10]))
+    return samples
 
+se_samples = get_samples(se_covar(x,x))
+per_samples = get_samples(per_covar(x,x))
+rq_samples = get_samples(rq_covar(x,x))
+
+plt.figure(figsize=(15,8))
+plt.subplot(231)
+plt.plot(x, se_samples.T, color='grey')
+
+plt.subplot(232)
+plt.plot(x, per_samples.T,color='grey')
+
+plt.subplot(233)
+plt.plot(x, rq_samples.T, color='grey')
 
 
 # Drawing functions from kernels with hyper priors (Forward sampling with hyper priors)
 
+os_prior = gpytorch.priors.LogNormalPrior(10,10)
+l_prior = gpytorch.priors.GammaPrior(20,0.5)
+
+se_covar = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel(lengthscale_prior=l_prior), outputscale_prior = os_prior)
+
+se_samples = get_samples(se_covar(x,x))
+
+per_samples = get_samples(per_covar(x,x))
+rq_samples = get_samples(rq_covar(x,x))
 
 
+plt.subplot(234)
+plt.plot(x, se_samples.T)
 
+plt.subplot(235)
+plt.plot(x, per_samples.T,color='grey')
 
-
+plt.subplot(236)
+plt.plot(x, rq_samples.T, color='grey')
 
---
+++ GP/pymc3/prior_sampling.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 28 16:14:44 2020
+
+@author: vidhi
+"""
+
+
+import numpy as np
+import math
+from matplotlib import pyplot as plt
+import theano.tensor as tt
+import pymc3 as pm
+import matplotlib.cm as cmap
+import arviz as arv
+
+# A one dimensional column vector of inputs.
+X = np.linspace(0, 10, 100)[:,None]
+
+with pm.Model() as fixed_model:
+
+       sig_sd = 2
+
+       # Specify the covariance functions
+       cov_func_se = pm.gp.cov.Constant(sig_sd**2)*pm.gp.cov.ExpQuad(1, ls=2)
+       cov_func_per = pm.gp.cov.Constant(sig_sd**2)*pm.gp.cov.Periodic(1, period=1, ls=2)
+       cov_func_mat = pm.gp.cov.Constant(sig_sd**2)*pm.gp.cov.Matern32(1, ls=2)
+
+       gp_se = pm.gp.Latent(cov_func=cov_func_se)
+       gp_per = pm.gp.Latent(cov_func=cov_func_per)
+       gp_mat = pm.gp.Latent(cov_func=cov_func_mat)
+
+       f_se = gp_se.prior("f_se", X=X)
+       f_per = gp_per.prior("f_per", X=X)
+       f_mat = gp_mat.prior("f_mat", X=X)
+
+       trace_fixed = pm.sample_prior_predictive(15, var_names=['f_se','f_per', 'f_mat'])
+
+
+with pm.Model() as hierarchical_model:
+
+       # prior on lengthscale
+       log_ls = pm.Gamma('log_ls',alpha = 2, beta = 1)
+       ls = pm.Deterministic('ls', tt.exp(log_ls))
+
+       # prior on period
+       log_p = pm.Gamma('log_p',alpha = 2, beta = 1)
+       p = pm.Deterministic('p', tt.exp(log_p))
+
+       #fixed signal variance
+       #sig_sd = pm.Gamma('sig_sd', mu=0, sigma=0.1)
+       sig_sd = 2
+
+       # Specify the covariance functions
+       cov_func_se = pm.gp.cov.Constant(sig_sd**2)*pm.gp.cov.ExpQuad(1, ls=ls)
+       cov_func_per = pm.gp.cov.Constant(sig_sd**2)*pm.gp.cov.Periodic(1, period=p, ls=ls)
+       cov_func_mat = pm.gp.cov.Constant(sig_sd**2)*pm.gp.cov.Matern32(1, ls=ls)
+
+       gp_se = pm.gp.Latent(cov_func=cov_func_se)
+       gp_per = pm.gp.Latent(cov_func=cov_func_per)
+       gp_mat = pm.gp.Latent(cov_func=cov_func_mat)
+
+       f_se = gp_se.prior("f_se", X=X)
+       f_per = gp_per.prior("f_per", X=X)
+       f_mat = gp_mat.prior("f_mat", X=X)
+
+       trace_hier = pm.sample_prior_predictive(15, var_names=['f_se','f_per', 'f_mat'])
+
+
+plt.figure(figsize=(15,10))
+plt.subplot(231)
+plt.plot(X, trace_fixed['f_se'].T, color='grey')
+plt.title('SE (ls=2)' )
+plt.subplot(232)
+plt.plot(X, trace_fixed['f_per'].T,color='grey')
+plt.title('Periodic (ls=2, p=1)')
+plt.subplot(233)
+plt.plot(X, trace_fixed['f_mat'].T,color='grey')
+plt.title('Matern32 (ls=2)')
+plt.subplot(234)
+plt.plot(X, trace_hier['f_se'].T)
+plt.title(r'$\mathcal{ls} \sim Gamma(2,1)$')
+plt.subplot(235)
+plt.plot(X, trace_hier['f_per'].T)
+plt.title(r'$\mathcal{ls} \sim Gamma(2,1)$' + '$\mathcal{p} \sim Gamma(2,1)$')
+plt.subplot(236)
+plt.plot(X, trace_hier['f_mat'].T)
+plt.title(r'$\mathcal{ls} \sim Gamma(2,1)$')
+plt.suptitle('Sampling from the prior')
+
+
+
+with pm.Model() as latent_gp_model:
+
+     # prior on lengthscale
+    log_ls = pm.Normal('log_ls',mu = 0, sd = 1)
+    ls = pm.Deterministic('ls', tt.exp(log_ls))
+
+    sig_sd = 2
+    # Specify the covariance function.
+    cov_func = pm.gp.cov.Constant(sig_sd**2)*pm.gp.cov.ExpQuad(1, ls=ls)
+
+    # Specify the GP.  The default mean function is `Zero`.
+    gp = pm.gp.Latent(cov_func=cov_func)
+
+    # Place a GP prior over the function f.
+    f = gp.prior("f", X=X)
+
+    trace = pm.sample(10)
