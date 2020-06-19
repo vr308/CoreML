7c6eaeff8c356e92d79feafe7020a0b260158fd3
Vidhi Lalchand <vidhilalchand@gmail.com>
2020-06-19 01:20:02 +0100

    learning pytorch

--- Neural Nets/neural_network.py
+++ Neural Nets/neural_network.py
@@ -234,3 +234,48 @@ for t in range(500):
     # Update weights using gradient descent
     w1 -= learning_rate * grad_w1
     w2 -= learning_rate * grad_w2
+    
+###########################
+# MLP using torch / autograd with RELU
+###########################
+
+import torch
+
+dtype = torch.float
+device = torch.device("cpu")
+# N is batch size; D_in is input dimension;
+# H is hidden dimension; D_out is output dimension.
+N, D_in, H, D_out = 64, 1000, 100, 10
+
+# Create random input and output data
+x = torch.randn(N, D_in, device=device, dtype=dtype)
+y = torch.randn(N, D_out, device=device, dtype=dtype)
+
+# Randomly initialize weights
+w1 = torch.randn(D_in, H, device=device, dtype=dtype, requires_grad=True)
+w2 = torch.randn(H, D_out, device=device, dtype=dtype, requires_grad=True)
+
+
+learning_rate = 1e-6
+for t in range(500):
+    # Forward pass: compute predicted y
+    y_pred = x.mm(w1).clamp(min=0).mm(w2)
+
+    # Compute and print loss using operations on Tensors.
+    # Now loss is a Tensor of shape (1,)
+    # loss.item() gets the scalar value held in the loss.
+    loss = (y_pred - y).pow(2).sum()
+    if t % 100 == 99:
+        print(t, loss.item())
+
+    # Backprop to compute gradients of w1 and w2 with respect to loss
+    loss.backward()
+
+    # Update weights using gradient descent
+    with torch.no_grad():
+        w1 -= learning_rate * w1.grad
+        w2 -= learning_rate * w2.grad
+
+        # Manually zero the gradients after updating weights
+        w1.grad.zero_()
+        w2.grad.zero_()
