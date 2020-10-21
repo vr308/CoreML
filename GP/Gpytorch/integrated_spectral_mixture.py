f87c42e99a8ad04d656b46dfb1564a1f1c181efa
Vidhi Lalchand <vidhilalchand@gmail.com>
2020-10-21 11:36:24 +0100

    edits

--- GP/Gpytorch/integrated_spectral_mixture.py
+++ GP/Gpytorch/integrated_spectral_mixture.py
@@ -8,25 +8,36 @@ Created on Sat May  2 19:52:06 2020
 
 import numpy as np
 import math 
+import matplotlib.pyplot as plt
+import scipy as sp
 
-x = np.linspace(-1,1,100)
+x = np.linspace(0.0,5,100)
 lam = 7
 mu = 1
 sig_f = 5
 l = 2
 alpha = 0.01
 
-def integrated_spectral_mixture(x, x0, sig_f, lam, mu, l, alpha):
-    tau = x - x0
-    pi_term = 2*np.pi*tau*mu
-    mu_int_term = np.divide(np.exp(-lam*mu), lam**2 + 4*(np.pi**2)*tau**2)*(-lam*np.cos(pi_term) + 2*np.pi*tau*np.sin(pi_term))
+def integrated_spectral_mixture(tau, sig_f, lam, mu, l, alpha):
+    #pi_term = 2*np.pi*tau*mu
+   # mu_int_term = np.divide(np.exp(-lam*mu), lam**2 + 4*(np.pi**2)*tau**2)*(-lam*np.cos(pi_term) + 2*np.pi*tau*np.sin(pi_term))
+    int_term = (lam*sig_f)**2*(1/(lam**2 + (2*np.pi*tau)**2))
     scale_term = np.power(1 + np.divide(2*(np.pi**2)*(tau**2), alpha*(l**2)), -alpha)
-    return -sig_f**2*(mu_int_term)*(scale_term)
+    return int_term*scale_term
 
-kf = integrated_spectral_mixture(x, x[0], sig_f, lam, mu, l, alpha)
+def integrated_spectral_mixture_uniform(tau, sig_f, b, a, l, alpha):
+    const_term = 1/(2*np.pi*(b-a))*sig_f**2
+    int_term = (np.sin(2*np.pi*tau*b) - np.sin(2*np.pi*tau*a))/tau
+    int_term[tau == 0] = 2*np.pi*(b-a)
+    rq_term = np.power(1 + np.divide(2*(np.pi**2)*(tau**2), alpha*(l**2)), -alpha)
+    return const_term*int_term*rq_term
+
+kf = integrated_spectral_mixture(x, sig_f, lam, mu, l, alpha)
+kfu = integrated_spectral_mixture_uniform(x, sig_f, 3, 2, l, alpha)
 
 columns = np.empty((100,100))
 for i in np.arange(100):
-    columns[i] = integrated_spectral_mixture(x, x[i], sig_f, lam, mu, l, alpha)
-    
+    columns[i] = integrated_spectral_mixture(x, sig_f, lam, mu, l, alpha)
+
+K = sp.linalg.toeplitz(kfu)
 plt.matshow(columns)
--- Misc/playing_with_kernels.py
+++ Misc/playing_with_kernels.py
@@ -7,16 +7,74 @@
 
 import numpy as np
 import pymc3 as pm 
-import matplotlib.pylab as plt
-import tensorflow as tf
+import seaborn as sns
 
-X = np.linspace(-10, 10, 1000)[:,None]
+X = np.linspace(-10, 10, 100)[:,None]
+y = (X**3 - X**2 + X)
 
 lengthscale = 2
 eta = 2.0
 cov = eta**2 * pm.gp.cov.ExpQuad(1, lengthscale)
 
 K = cov(X).eval()
+plt.matshow(K)
+
+def warp_func(x, a, b, c):
+    return 1.0 + x + (a * tt.tanh(b * (x - c)))
+
+
+def logistic(x, a, x0, c, d):
+    # a is the slope, x0 is the location
+    return d * pm.math.invlogit(a*(x - x0)) + c
+c = 0.1
+d = 2.0
+
+a = 2.0
+import theano.tensor as tt
+import matplotlib.pylab as plt
+x0 = 5.0
+cov1 = pm.gp.cov.ScaledCov(1, scaling_func=logistic, args=(-a, x0, c, d),
+                           cov_func=pm.gp.cov.ExpQuad(1, 0.2))
+cov2 = pm.gp.cov.ScaledCov(1, scaling_func=logistic, args=(a, x0, c, d),
+                           cov_func=pm.gp.cov.Cosine(1, 0.5))
+cov = cov1 + cov2
+K = cov(X).eval()
 
 plt.matshow(K)
+
+plt.figure(figsize=(14,4))
+plt.fill_between(X.flatten(), np.zeros(100), logistic(X.flatten(), -a, x0, c, d).eval(), label="ExpQuad region",
+                 color="slateblue", alpha=0.4);
+plt.fill_between(X.flatten(), np.zeros(100), logistic(X.flatten(), a, x0, c, d).eval(), label="Cosine region",
+                 color="firebrick", alpha=0.4);
+plt.legend();
+plt.xlabel("X"); plt.ylabel("$\phi(x)$");
+plt.title("The two scaling functions");
+
+plt.figure(figsize=(14,4))
+plt.plot(X, pm.MvNormal.dist(mu=np.zeros(K.shape[0]), cov=K).random(size=3).T);
+plt.title("Samples from the GP prior");
+plt.ylabel("y");
+plt.xlabel("X");
+
+X_gaussian = pm.MvNormal.dist(mu=np.zeros(2), cov=np.eye(2)).random(size=100)
+sns.kdeplot(X_gaussian, shade=True, bw=2)
+
+lengthscale = [10,0.1]
+eta = 2.0
+cov_se = eta**2 * pm.gp.cov.ExpQuad(2, lengthscale)
+cov_per = eta**2 * pm.gp.cov.Periodic(2, period=1, ls=lengthscale)
+
+K_rbf = cov_se(X_gaussian).eval()
+K_per = cov(X_gaussian).eval()
+
+K = K_per
+
+warping_func1 = pm.MvNormal.dist(mu=np.zeros(K_per.shape[0]), cov=K_per).random(size=1).T
+warping_func2 = pm.MvNormal.dist(mu=np.zeros(K_per.shape[0]), cov=K_per).random(size=1).T
+
+sns.kdeplot(np.vstack((warping_func1, warping_func2)).T, shade=2)
+plt.plot(warping_func1, warping_func2, 'ro')
+
+# 
 
